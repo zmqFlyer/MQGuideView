8a3d914dbc43758c8b6cc7d574a0b91d71671562
zmq <zhangmengqi@iresearch.com.cn>
2016-09-22 15:50:29 +0800

    Initial Commit

---
+++ MQGuideView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MQGuideView
+//
+//  Created by irs on 16/9/22.
+//  Copyright © 2016年 zmq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MQGuideView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MQGuideView
+//
+//  Created by irs on 16/9/22.
+//  Copyright © 2016年 zmq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
